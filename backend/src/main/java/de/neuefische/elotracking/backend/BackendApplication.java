f986602ba3eaa582a0e23d80efb6d9e72d55407e
JakobHeiden <JakobHeiden@gmx.de>
2020-11-17 10:59:59 +0100

    Update BackendApplication.java

--- backend/src/main/java/de/neuefische/elotracking/backend/BackendApplication.java
+++ backend/src/main/java/de/neuefische/elotracking/backend/BackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-        //EloTrackingService elo = new EloTrackingService(new DiscordBot());
 	    SpringApplication.run(BackendApplication.class, args);
 	}
 
