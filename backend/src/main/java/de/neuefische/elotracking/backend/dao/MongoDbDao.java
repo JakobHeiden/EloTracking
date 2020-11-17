af9b64950a02a05a0c2b8ca9c2502a1af63eeaf0
Jakob Heiden <JakobHeiden@gmx.de>
2020-11-17 13:57:51 +0100

    wip

---
+++ backend/src/main/java/de/neuefische/elotracking/backend/dao/MongoDbDao.java
@@ -0,0 +1,2 @@
+package de.neuefische.elotracking.backend.dao;public interface MongoDbDao {
+}
---
+++ backend/src/main/java/de/neuefische/elotracking/backend/model/Dummy.java
@@ -0,0 +1,12 @@
+package de.neuefische.elotracking.backend.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dummy {
+    private String data;
+}
