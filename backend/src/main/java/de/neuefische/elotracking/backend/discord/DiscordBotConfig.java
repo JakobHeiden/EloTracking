a972ce7d348ecb258155282762d670603aa3df05
JakobHeiden <JakobHeiden@gmx.de>
2020-11-17 11:03:55 +0100

    Add Discord4J dependency. Add Discord client to Spring (#5)
    * Add Discord4J dependency. Add Discord client to Spring
    
    * Update BackendApplication.java
    
    * Update DiscordBot.java

---
+++ backend/src/main/java/de/neuefische/elotracking/backend/discord/DiscordBotConfig.java
@@ -0,0 +1,21 @@
+package de.neuefische.elotracking.backend.discord;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DiscordBotConfig {
+
+    @Bean
+    public GatewayDiscordClient createClient() {
+        GatewayDiscordClient client = DiscordClientBuilder
+                .create(System.getenv("DISCORD_BOT_TOKEN"))
+                .build()
+                .login()
+                .block();
+        client.onDisconnect().block();
+        return client;
+    }
+}
