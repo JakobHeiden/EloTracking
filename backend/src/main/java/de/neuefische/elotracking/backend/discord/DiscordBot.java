97029bec68b30eaa973867cfcb709b4d0c24662d
JakobHeiden <JakobHeiden@gmx.de>
2020-11-20 13:53:45 +0100

    Add prototype functionality to scan for a command in discord, then write to the database, then in return write a message in discord (#10)

--- backend/src/main/java/de/neuefische/elotracking/backend/discord/DiscordBot.java
+++ backend/src/main/java/de/neuefische/elotracking/backend/discord/DiscordBot.java
@@ -1,7 +1,11 @@
 package de.neuefische.elotracking.backend.discord;
 
+import de.neuefische.elotracking.backend.model.Dummy;
+import de.neuefische.elotracking.backend.service.EloTrackingService;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,9 +13,18 @@ public class DiscordBot {
     private final GatewayDiscordClient gatewayDiscordClient;
 
     @Autowired
-    public DiscordBot(GatewayDiscordClient gatewayDiscordClient) {
+    public DiscordBot(GatewayDiscordClient gatewayDiscordClient, @Lazy EloTrackingService eloTrackingService) {
         this.gatewayDiscordClient = gatewayDiscordClient;
-    }
 
-    //hier Funktionen
+        //ping the mongodb. TODO remove later on
+        gatewayDiscordClient.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getContent().startsWith("!"))
+                .flatMap(message -> {
+                    Dummy serviceResponse = eloTrackingService.ping(message.getContent());
+                    return message.getChannel()
+                            .flatMap(channel -> channel.createMessage(serviceResponse.toString()));
+                })
+                .subscribe();
+    }
 }
