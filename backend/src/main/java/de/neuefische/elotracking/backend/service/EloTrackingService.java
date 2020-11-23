38a65bf6cc3728ba55fb34eac2706f1410c20857
Jakob Heiden <JakobHeiden@gmx.de>
2020-11-23 15:21:23 +0100

    Add config.txt, parse it for Properties

--- backend/src/main/java/de/neuefische/elotracking/backend/service/EloTrackingService.java
+++ backend/src/main/java/de/neuefische/elotracking/backend/service/EloTrackingService.java
@@ -3,18 +3,31 @@ package de.neuefische.elotracking.backend.service;
 import de.neuefische.elotracking.backend.dao.MongoDbDao;
 import de.neuefische.elotracking.backend.discord.DiscordBot;
 import de.neuefische.elotracking.backend.model.Dummy;
+import discord4j.core.object.entity.channel.PrivateChannel;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.tinylog.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 @Service
 public class EloTrackingService {
     private final DiscordBot discordBot;
     private final MongoDbDao mongoDbDao;
+    @Getter
+    private Properties config = new Properties();
+    private PrivateChannel adminDm;
 
     @Autowired
-    public EloTrackingService(DiscordBot discordBot, MongoDbDao mongoDbDao) {
+    public EloTrackingService(DiscordBot discordBot, MongoDbDao mongoDbDao) throws IOException {
         this.discordBot = discordBot;
         this.mongoDbDao = mongoDbDao;
+        config.load(new FileReader("backend/src/main/resources/config.txt"));
+        Logger.info(config.getProperty("ADMIN_DISCORD_ID"));
     }
 
     //TODO remove later on
