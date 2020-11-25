c0d21b7d24ae765b3e6cd62dce23198bc3d26f02
JakobHeiden <JakobHeiden@gmx.de>
2020-11-25 09:56:42 +0100

    Send logs to admin simple (#23)
    * Add config.txt, parse it for Properties
    
    * Open admin dm channel
    
    * Configure tinylog
    
    * Clean up imports
    
    * Clean up comments
    
    * Add sendToAdmin() method
    
    * Refactor central variable names to be much shorter

--- backend/src/main/java/de/neuefische/elotracking/backend/service/EloTrackingService.java
+++ backend/src/main/java/de/neuefische/elotracking/backend/service/EloTrackingService.java
@@ -3,18 +3,27 @@ package de.neuefische.elotracking.backend.service;
 import de.neuefische.elotracking.backend.dao.MongoDbDao;
 import de.neuefische.elotracking.backend.discord.DiscordBot;
 import de.neuefische.elotracking.backend.model.Dummy;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 @Service
 public class EloTrackingService {
-    private final DiscordBot discordBot;
+    private final DiscordBot bot;
     private final MongoDbDao mongoDbDao;
+    @Getter
+    private Properties config = new Properties();
 
     @Autowired
-    public EloTrackingService(DiscordBot discordBot, MongoDbDao mongoDbDao) {
-        this.discordBot = discordBot;
+    public EloTrackingService(@Lazy DiscordBot discordBot, MongoDbDao mongoDbDao) throws IOException {
+        this.bot = discordBot;
         this.mongoDbDao = mongoDbDao;
+        config.load(new FileReader("backend/src/main/resources/config.txt"));
     }
 
     //TODO remove later on
