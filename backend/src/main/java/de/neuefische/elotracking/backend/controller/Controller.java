53f5127bd9e272ea5fa4e264af9de0b19ed57b37
JakobHeiden <JakobHeiden@gmx.de>
2020-12-02 18:01:16 +0100

    Add controller (#34)
    * Update Challenge model
    
    * Add challenge, accept, win/lose commands
    
    * Refactor model update logic into Challenge itself
    
    * Add calculateElo()
    
    * Add new Players to database during challenge/accept
    
    * Update Player to have different id on different channels
    
    * Add updateRatings()
    
    * Finish report functionality
    
    * Clean up some code. Remove some error checking that might not be needed
    
    * Make service actually update Match.hasUpdatedPlayerRatings
    
    * Add Controller.getRankings(), EloTrackingService.getRankings()
    
    * Add controller

---
+++ backend/src/main/java/de/neuefische/elotracking/backend/controller/Controller.java
@@ -0,0 +1,25 @@
+package de.neuefische.elotracking.backend.controller;
+
+import de.neuefische.elotracking.backend.dto.PlayerInRankingsDto;
+import de.neuefische.elotracking.backend.service.EloTrackingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    private final EloTrackingService service;
+
+    public Controller(EloTrackingService eloTrackingService) {
+        this.service = eloTrackingService;
+    }
+
+    @GetMapping("{channelId}")
+    public List<PlayerInRankingsDto> getRankings(@PathVariable String channelId) {
+        return service.getRankings(channelId);
+    }
+}
