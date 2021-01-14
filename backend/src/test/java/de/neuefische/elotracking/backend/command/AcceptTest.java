d56573897f16dac72a643d3620321af2099f5fa3
Jakob Heiden <JakobHeiden@gmx.de>
2021-01-14 20:48:42 +0100

    Finish AcceptTest

--- backend/src/test/java/de/neuefische/elotracking/backend/command/AcceptTest.java
+++ backend/src/test/java/de/neuefische/elotracking/backend/command/AcceptTest.java
@@ -2,6 +2,7 @@ package de.neuefische.elotracking.backend.command;
 
 import de.neuefische.elotracking.backend.discord.DiscordBot;
 import de.neuefische.elotracking.backend.model.ChallengeModel;
+import de.neuefische.elotracking.backend.model.Game;
 import de.neuefische.elotracking.backend.service.EloTrackingService;
 import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.Message;
@@ -14,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -28,23 +30,27 @@ public class AcceptTest {
     final Snowflake authorSnowflake = mock(Snowflake.class);
     final Channel channel = mock(Channel.class);
     final Snowflake channelSnowflake = mock(Snowflake.class);
-    final Command accept = new Accept(bot, service, msg, channel);
 
     final String authorId = "1";
     final String channelId = "2";
+    final String challengerId = "3";
+    final Game game = new Game(channelId, "testgame");
+    final Command accept = new Accept(bot, service, msg, channel);
 
     @BeforeEach
     void setupMockup() {
+        when(service.findGameByChannelId(channelId)).thenReturn(Optional.of(game));
         when(msg.getAuthor()).thenReturn(Optional.of(author));
-        //when(author.getId()).thenReturn(authorSnowflake);
-        //when(authorSnowflake.asString()).thenReturn(authorId);
+        when(author.getId()).thenReturn(authorSnowflake);
+        when(authorSnowflake.asString()).thenReturn(authorId);
         when(channel.getId()).thenReturn(channelSnowflake);
-        //when(channelSnowflake.asString()).thenReturn(channelId);
+        when(channelSnowflake.asString()).thenReturn(channelId);
     }
 
     @Test
     @DisplayName("Accepting when no open challenge is present should not lead to function calls")
-    void noOpenChallenge() {
+    void noOpenChallengePresent() {
+        //arrange
         when(service.findChallengesOfPlayerForChannel(authorId, channelId))
                 .thenReturn(new LinkedList<ChallengeModel>());
 
@@ -52,8 +58,26 @@ public class AcceptTest {
         accept.execute();
 
         //assert
-        verify(service, never()).addNewPlayerIfPlayerNotPresent(anyString(), anyString());
+        verify(service, never()).addNewPlayerIfPlayerNotPresent(channelId, authorId);
         verify(service, never()).saveChallenge(any(ChallengeModel.class));
+    }
+
+    @Test
+    @DisplayName("Accepting an open challenge should lead to function calls")
+    void openChallengePresent() {
+        //arrange
+        List listOfChallenges = new LinkedList<ChallengeModel>();
+        ChallengeModel challenge = new ChallengeModel(channelId, challengerId, authorId);
+        listOfChallenges.add(challenge);
+        when(service.findChallengesOfPlayerForChannel(authorId, channelId))
+                .thenReturn(listOfChallenges);
+
+        //act
+        accept.execute();
+
+        //assert
+        verify(service).addNewPlayerIfPlayerNotPresent(channelId, authorId);
+        verify(service).saveChallenge(challenge);
     }
 
 
