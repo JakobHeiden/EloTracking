bab1fb0cbb0ca62cbe1a6a174bc1d08cd6acf0d8
Jakob Heiden <JakobHeiden@gmx.de>
2021-09-14 14:17:28 +0200

    Refactor some bean configurations

---
+++ backend/src/main/java/de/neuefische/elotracking/backend/service/GatewayDiscordClientConfiguration.java
@@ -0,0 +1,32 @@
+package de.neuefische.elotracking.backend.service;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class GatewayDiscordClientConfiguration {
+
+    @Bean
+    public GatewayDiscordClient createClient() {
+        GatewayDiscordClient client = DiscordClientBuilder
+                .create(System.getenv("DISCORD_BOT_TOKEN"))
+                .build()
+                .login()
+                .block();
+
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    User self = event.getSelf();
+                    log.info("Logged in as {}#{}", self.getUsername(), self.getDiscriminator());
+                });
+
+        return client;
+    }
+
+}
