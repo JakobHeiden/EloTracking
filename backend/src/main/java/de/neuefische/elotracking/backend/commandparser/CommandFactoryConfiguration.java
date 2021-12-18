8648cba1282b504df639259219f772dc913eb0e3
Jakob Heiden <JakobHeiden@gmx.de>
2021-12-18 01:27:26 +0100

    make clean structure for client->Parser->Command

--- backend/src/main/java/de/neuefische/elotracking/backend/commandparser/CommandFactoryConfiguration.java
+++ backend/src/main/java/de/neuefische/elotracking/backend/commandparser/CommandFactoryConfiguration.java
@@ -1,16 +1,7 @@
 package de.neuefische.elotracking.backend.commandparser;
 
 import de.neuefische.elotracking.backend.commands.Command;
-import de.neuefische.elotracking.backend.configuration.CommandAbbreviationMapper;
-import de.neuefische.elotracking.backend.service.DiscordBotService;
-import de.neuefische.elotracking.backend.service.EloTrackingService;
-import de.neuefische.elotracking.backend.timedtask.TimedTaskQueue;
-import discord4j.core.event.domain.Event;
-import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
-import discord4j.core.event.domain.message.ReactionAddEvent;
-import discord4j.core.object.entity.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -21,62 +12,25 @@ import java.util.function.Function;
 @Configuration
 public class CommandFactoryConfiguration {
 
-	@Autowired
-    CommandAbbreviationMapper commandAbbreviationMapper;
-
 	@Bean
-	public Function<MessageWrapper, Command> commandFactory() {
-		return msgWrapper -> createCommand(msgWrapper);
+	public Function<ApplicationCommandInteractionEventWrapper, Command> slashCommandFactory() {
+		return eventWrapper -> createSlashCommand(eventWrapper);
 	}
 
 	@Bean
-	public Function<EventWrapper, Command> slashCommandFactory() {
-		return eventWrapper -> createCommand(eventWrapper);
-	}
-
-	@Bean
-	@Scope("prototype")
-	public Command createCommand(MessageWrapper msgWrapper) {
-		Message msg = msgWrapper.msg();
-		String commandString = msg.getContent().split(" ")[0].substring(1).toLowerCase();
-		log.trace("commandString = " + commandString);
-		commandString = commandAbbreviationMapper.mapIfApplicable(commandString);
-		String commandClassName = commandString.substring(0, 1).toUpperCase() + commandString.substring(1);
-		try {
-			return (Command) Class.forName("de.neuefische.elotracking.backend.commands." + commandClassName)
-					.getConstructor(Message.class, EloTrackingService.class, DiscordBotService.class, TimedTaskQueue.class)
-					.newInstance(msg, msgWrapper.service(), msgWrapper.bot(), msgWrapper.queue());
-		} catch (Exception e) {//TODO
-			if (e.getClass().equals(ClassNotFoundException.class)) {
-				return null;//new Unknown(msg, msgWrapper.service(), msgWrapper.bot(), msgWrapper.queue());
-			} else {
-				e.printStackTrace();//TODO
-				return null;
-			}
-		}
+	public Function<ReactionAddEventWrapper, Command> emojiCommandFactory() {
+		return eventWrapper -> createEmojiCommand(eventWrapper);
 	}
 
 	@Bean
 	@Scope("prototype")
-	public Command createCommand(EventWrapper eventWrapper) {
-		Event event = eventWrapper.getEvent();
-		String commandClassName = "commandStringNotSet";
-		if (event instanceof ApplicationCommandInteractionEvent) {
-			commandClassName = ((ApplicationCommandInteractionEvent) event).getCommandName();
-		}
-		if (event instanceof ReactionAddEvent) {
-			commandClassName = eventWrapper.getCommandString();
-		}
-		commandClassName = commandClassName.substring(0, 1).toUpperCase() + commandClassName.substring(1);
-		log.trace("commandString = " + commandClassName);
-		try {
-			return (Command) Class.forName("de.neuefische.elotracking.backend.commands." + commandClassName)
-					.getConstructor(Event.class, EloTrackingService.class, DiscordBotService.class, TimedTaskQueue.class)
-					.newInstance(event, eventWrapper.getService(), eventWrapper.getBot(), eventWrapper.getQueue());
-		} catch (Exception e) {
-			eventWrapper.getBot().sendToAdmin(e.getMessage());
-			e.printStackTrace();
-			return null;
-		}
+	public Command createSlashCommand(ApplicationCommandInteractionEventWrapper eventWrapper) {
+		return CommandParser.createSlashCommand(eventWrapper);
+	}
+
+	@Bean
+	@Scope("prototype")
+	public Command createEmojiCommand(ReactionAddEventWrapper eventWrapper) {
+		return CommandParser.createEmojiCommand(eventWrapper);
 	}
 }
--- backend/src/main/java/de/neuefische/elotracking/backend/commandparser/CommandParser.java
+++ backend/src/main/java/de/neuefische/elotracking/backend/commandparser/CommandParser.java
@@ -1,11 +1,14 @@
 package de.neuefische.elotracking.backend.commandparser;
 
+import de.neuefische.elotracking.backend.commands.Accept;
 import de.neuefische.elotracking.backend.commands.Command;
+import de.neuefische.elotracking.backend.commands.Win;
 import de.neuefische.elotracking.backend.service.DiscordBotService;
 import de.neuefische.elotracking.backend.service.EloTrackingService;
 import de.neuefische.elotracking.backend.timedtask.TimedTaskQueue;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
 import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.object.command.ApplicationCommandOption;
@@ -24,7 +27,8 @@ import java.util.function.Function;
 public class CommandParser {
 
     private final GatewayDiscordClient client;
-    private final Function<EventWrapper, Command> commandFactory;
+    private final Function<ApplicationCommandInteractionEventWrapper, Command> slashCommandFactory;
+    private final Function<ReactionAddEventWrapper, Command> emojiCommandFactory;
     private final EloTrackingService service;
     private final DiscordBotService bot;
     private final TimedTaskQueue queue;
@@ -32,10 +36,12 @@ public class CommandParser {
     private final long entenwieseId = 612347065762054165L;
     private final Snowflake botSnowflake;
 
-    public CommandParser(GatewayDiscordClient client, EloTrackingService service, DiscordBotService bot,
-                         TimedTaskQueue queue, Function<EventWrapper, Command> commandFactory) {
+    public CommandParser(GatewayDiscordClient client, EloTrackingService service, DiscordBotService bot, TimedTaskQueue queue,
+                         Function<ApplicationCommandInteractionEventWrapper, Command> slashCommandFactory,
+                         Function<ReactionAddEventWrapper, Command> emojiCommandFactory) {
         this.client = client;
-        this.commandFactory = commandFactory;
+        this.slashCommandFactory = slashCommandFactory;
+        this.emojiCommandFactory = emojiCommandFactory;
         this.service = service;
         this.bot = bot;
         this.queue = queue;
@@ -60,7 +66,7 @@ public class CommandParser {
         applicationService.createGuildApplicationCommand(applicationId, entenwieseId, challengeUserCommandRequest).subscribe();
 
         client.on(ApplicationCommandInteractionEvent.class, event -> {
-            Command command = commandFactory.apply(new EventWrapper(event, service, bot, this.queue));
+            Command command = slashCommandFactory.apply(new ApplicationCommandInteractionEventWrapper(event, service, bot, queue));
             command.execute();
             Mono returnValue = null;
             for (String reply : command.getBotReplies()) {
@@ -71,26 +77,51 @@ public class CommandParser {
 
         client.on(ReactionAddEvent.class)
                 .filter(event -> !event.getUserId().equals(botSnowflake))
-                .filter(event -> event.getEmoji().equals(Emojis.checkMark) || event.getEmoji().equals(Emojis.crossMark))
-                .filter(event -> service.challengeExistsByChallengerMessageId(event.getMessageId().asLong()) // TODO optimieren?
-                        || service.challengeExistsByAcceptorMessageId(event.getMessageId().asLong()))
-                .map(event -> new EventWrapper(event, service, bot, queue))
-                .map(this::setCommandStringForReactionAddEvent)
-                .map(commandFactory::apply)
+                .filter(event -> event.getMessage().block()// TODO langsam. vllt message ids cachen?
+                        .getAuthor().get().getId().equals(botSnowflake))
+                .map(event -> new ReactionAddEventWrapper(event, service, bot, queue))
+                .map(emojiCommandFactory::apply)
+                .filter(command -> command == null)
                 .subscribe(Command::execute);
     }
 
-    public EventWrapper setCommandStringForReactionAddEvent(EventWrapper wrapper) {
-        String lastLine = ((ReactionAddEvent) wrapper.getEvent()).getMessage().block()
-                .getContent().split("/n")[0];// TODO vllt langsam. vllt doch ueber event.getMessageId()?
-        ReactionEmoji emoji = ((ReactionAddEvent) wrapper.getEvent()).getEmoji();
+    public static Command createSlashCommand(ApplicationCommandInteractionEventWrapper eventWrapper) {
+        String commandClassName = eventWrapper.event().getCommandName();
+        commandClassName = commandClassName.substring(0, 1).toUpperCase() + commandClassName.substring(1);
+        log.trace("commandString = " + commandClassName);
+        try {
+            return (Command) Class.forName("de.neuefische.elotracking.backend.commands." + commandClassName)
+                    .getConstructor(Event.class, EloTrackingService.class, DiscordBotService.class, TimedTaskQueue.class)
+                    .newInstance(eventWrapper.event(), eventWrapper.service(), eventWrapper.bot(), eventWrapper.queue());
+        } catch (Exception e) {
+            eventWrapper.bot().sendToAdmin(e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-        if (lastLine.contains("Accept?") && emoji.equals(Emojis.checkMark)) wrapper.setCommandString("accept");
+    public static Command createEmojiCommand(ReactionAddEventWrapper wrapper) {
+        ReactionAddEvent event = wrapper.event();
+        EloTrackingService service = wrapper.service();
+        DiscordBotService bot = wrapper.bot();
+        TimedTaskQueue queue = wrapper.queue();
+        String lastLine = event.getMessage().block()// TODO vllt langsam. vllt doch ueber event.getMessageId()?
+                .getContent().split("/n")[0];    // entweder ich gehe ueber den content (langsam) oder ich schaue in die db (auch langsam)
+        ReactionEmoji emoji = wrapper.event().getEmoji();
 
-        if (lastLine.contains("won :arrow_up: or lost :arrow_down:") && emoji.equals(Emojis.arrowUp)) wrapper.setCommandString("win");
-        if (lastLine.contains("won :arrow_up: or lost :arrow_down:") && emoji.equals(Emojis.arrowDown)) wrapper.setCommandString("lose");
-        if (lastLine.contains("won :arrow_up: or lost :arrow_down:") && emoji.equals(Emojis.leftRightArrow)) wrapper.setCommandString("draw");
-        if (lastLine.contains("won :arrow_up: or lost :arrow_down:") && emoji.equals(Emojis.crossMark)) wrapper.setCommandString("cancel");
-        return wrapper;
+        if (wrapper.service().challengeExistsByChallengerMessageId(event.getMessageId().asLong())
+                || wrapper.service().challengeExistsByAcceptorMessageId(event.getMessageId().asLong())) {
+            if (lastLine.contains("Accept?")) {
+                if (emoji.equals(Emojis.checkMark)) return new Accept(event, service, bot, queue);
+                if (emoji.equals(Emojis.crossMark)) return null;//TODO reject
+            }
+            if (lastLine.contains("won :arrow_up: or lost :arrow_down:")) {
+                if (emoji.equals(Emojis.arrowUp)) return new Win(event, service, bot, queue);
+                if (emoji.equals(Emojis.arrowDown)) return null;// new Lose(event, service, bot, queue);
+                if (emoji.equals(Emojis.leftRightArrow)) return null;
+                if (emoji.equals(Emojis.crossMark)) return null;
+            }
+        }
+        return null;
     }
 }
