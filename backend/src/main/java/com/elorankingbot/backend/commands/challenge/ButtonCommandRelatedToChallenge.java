39d7d7a44c79ada9e1cb5df53f56bb7addfe87bd
Jakob Heiden <JakobHeiden@gmx.de>
2022-02-10 14:58:52 +0100

    remove ButtonCommandRelatedToChallengeOrDispute

--- backend/src/main/java/com/elorankingbot/backend/commands/challenge/ButtonCommandRelatedToChallenge.java
+++ backend/src/main/java/com/elorankingbot/backend/commands/challenge/ButtonCommandRelatedToChallenge.java
@@ -1,6 +1,8 @@
 package com.elorankingbot.backend.commands.challenge;
 
-import com.elorankingbot.backend.commands.ButtonCommandRelatedToChallengeOrDispute;
+import com.elorankingbot.backend.commands.ButtonCommand;
+import com.elorankingbot.backend.model.ChallengeModel;
+import com.elorankingbot.backend.model.Game;
 import com.elorankingbot.backend.service.DiscordBotService;
 import com.elorankingbot.backend.service.EloRankingService;
 import com.elorankingbot.backend.timedtask.TimedTaskQueue;
@@ -8,15 +10,21 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 import discord4j.core.object.entity.Message;
 
-public abstract class ButtonCommandRelatedToChallenge extends ButtonCommandRelatedToChallengeOrDispute {
+public abstract class ButtonCommandRelatedToChallenge extends ButtonCommand {
 
 	protected final Message parentMessage;
 	protected final Message targetMessage;
 	protected final boolean isChallengerCommand;
+	protected final ChallengeModel challenge;
+	protected final long guildId;
+	protected final Game game;
 
 	protected ButtonCommandRelatedToChallenge(ButtonInteractionEvent event, EloRankingService service, DiscordBotService bot,
 											  TimedTaskQueue queue, GatewayDiscordClient client) {
 		super(event, service, bot, queue, client);
+		this.challenge = service.findChallengeById(Long.parseLong(event.getCustomId().split(":")[1])).get();
+		this.guildId = challenge.getGuildId();
+		this.game = service.findGameByGuildId(guildId).get();
 		if (event.getInteraction().getChannelId().asLong() == challenge.getChallengerChannelId()) {
 			this.isChallengerCommand = true;
 		} else {
@@ -31,5 +39,15 @@ public abstract class ButtonCommandRelatedToChallenge extends ButtonCommandRelat
 	protected void updateAndSaveChallenge(Message message) {// TODO vllt in interface, default method refaktorn
 		if (isChallengerCommand) updateAcceptorMessageIdAndSaveChallenge(message);
 		else updateChallengerMessageIdAndSaveChallenge(message);
+	}
+
+	protected void updateChallengerMessageIdAndSaveChallenge(Message message) {
+		challenge.setChallengerMessageId(message.getId().asLong());
+		service.saveChallenge(challenge);
+	}
+
+	protected void updateAcceptorMessageIdAndSaveChallenge(Message message) {
+		challenge.setAcceptorMessageId(message.getId().asLong());
+		service.saveChallenge(challenge);
 	}
 }
--- backend/src/main/java/com/elorankingbot/backend/commands/dispute/ButtonCommandRelatedToDispute.java
+++ backend/src/main/java/com/elorankingbot/backend/commands/dispute/ButtonCommandRelatedToDispute.java
@@ -1,6 +1,8 @@
 package com.elorankingbot.backend.commands.dispute;
 
-import com.elorankingbot.backend.commands.ButtonCommandRelatedToChallengeOrDispute;
+import com.elorankingbot.backend.commands.ButtonCommand;
+import com.elorankingbot.backend.model.ChallengeModel;
+import com.elorankingbot.backend.model.Game;
 import com.elorankingbot.backend.model.Match;
 import com.elorankingbot.backend.service.DiscordBotService;
 import com.elorankingbot.backend.service.EloRankingService;
@@ -14,15 +16,21 @@ import discord4j.core.object.component.ActionRow;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.TextChannel;
 
-public abstract class ButtonCommandRelatedToDispute extends ButtonCommandRelatedToChallengeOrDispute {
+public abstract class ButtonCommandRelatedToDispute extends ButtonCommand {
 
 	protected TextChannel disputeChannel;
 	protected String moderatorName;
 	protected Message challengerMessage;
 	protected Message acceptorMessage;
+	protected final ChallengeModel challenge;
+	protected final long guildId;
+	protected final Game game;
 
 	protected ButtonCommandRelatedToDispute(ButtonInteractionEvent event, EloRankingService service, DiscordBotService bot, TimedTaskQueue queue, GatewayDiscordClient client) {
 		super(event, service, bot, queue, client);
+		this.challenge = service.findChallengeById(Long.parseLong(event.getCustomId().split(":")[1])).get();
+		this.guildId = challenge.getGuildId();
+		this.game = service.findGameByGuildId(guildId).get();
 		this.disputeChannel = (TextChannel) event.getInteraction().getChannel().block();
 		this.moderatorName = event.getInteraction().getUser().getUsername();
 		this.challengerMessage = client.getMessageById(Snowflake.of(challenge.getChallengerChannelId()),
@@ -53,5 +61,15 @@ public abstract class ButtonCommandRelatedToDispute extends ButtonCommandRelated
 				challenge.getChallengerMessageId(), challenge.getChallengerChannelId(), match);
 		queue.addTimedTask(TimedTask.TimedTaskType.MATCH_SUMMARIZE, game.getMessageCleanupTime(),
 				challenge.getAcceptorMessageId(), challenge.getAcceptorChannelId(), match);
+	}
+
+	protected void updateChallengerMessageIdAndSaveChallenge(Message message) {
+		challenge.setChallengerMessageId(message.getId().asLong());
+		service.saveChallenge(challenge);
+	}
+
+	protected void updateAcceptorMessageIdAndSaveChallenge(Message message) {
+		challenge.setAcceptorMessageId(message.getId().asLong());
+		service.saveChallenge(challenge);
 	}
 }
