0fedb6eba465cd829173cd39f8493c0e1a4de966
Jakob Heiden <JakobHeiden@gmx.de>
2022-03-20 23:16:55 +0100

    add RuleAsCancel

--- backend/src/main/java/com/elorankingbot/backend/commands/mod/dispute/RuleAsCancel.java
+++ backend/src/main/java/com/elorankingbot/backend/commands/mod/dispute/RuleAsCancel.java
@@ -1,7 +1,10 @@
 package com.elorankingbot.backend.commands.mod.dispute;
 
+import com.elorankingbot.backend.model.Player;
 import com.elorankingbot.backend.service.Services;
+import com.elorankingbot.backend.tools.EmbedBuilder;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
+import discord4j.core.spec.EmbedCreateSpec;
 
 public class RuleAsCancel extends ButtonCommandRelatedToDispute {
 
@@ -12,33 +15,25 @@ public class RuleAsCancel extends ButtonCommandRelatedToDispute {
 	public void execute() {
 		if (!isByModeratorOrAdminDoReply()) return;
 
-		//TODO!
-		/*
+		dbservice.deleteMatch(match);
 
-		dbservice.deleteChallenge(challenge);
+		postToDisputeChannelAndUpdateButtons(String.format("%s has ruled the match to be canceled.", moderatorName));
 
-		postToDisputeChannelAndUpdateButtons(String.format(
-				"%s has ruled the challenge to be canceled. <@%s> <@%s>",
-				moderatorName, challenge.getChallengerUserId(), challenge.getAcceptorUserId()));
-		new MessageUpdater(challengerMessage)
-				.addLine(String.format("%s has ruled this match to be canceled :negative_squared_cross_mark:.", moderatorName))
-				.makeAllItalic()
-				.resend()
-				.withComponents(none)
-				.block();
-		new MessageUpdater(acceptorMessage)
-				.addLine(String.format("%s has ruled this match to be canceled :negative_squared_cross_mark:.", moderatorName))
-				.makeAllItalic()
-				.resend()
-				.withComponents(none)
-				.subscribe();
+		for (Player player : match.getPlayers()) {
+			bot.getPlayerMessage(player, match)
+					.subscribe(message -> {
+						String embedTitle = String.format("%s has ruled the match to be canceled.", moderatorName);
+						EmbedCreateSpec embedCreateSpec = EmbedBuilder.createMatchEmbed(embedTitle, match, player.getTag());
+						message.delete().subscribe();
+						bot.getPrivateChannelByUserId(player.getUserId()).subscribe(channel ->
+								channel.createMessage(embedCreateSpec).subscribe());
+					});
+		}
 
 		//queue.addTimedTask(TimedTask.TimedTaskType.MESSAGE_DELETE, game.getMessageCleanupTime(),
 		//		challengerMessage.getId().asLong(), challengerMessage.getChannelId().asLong(), null);
 		//queue.addTimedTask(TimedTask.TimedTaskType.MESSAGE_DELETE, game.getMessageCleanupTime(),
 		//		acceptorMessage.getId().asLong(), acceptorMessage.getChannelId().asLong(), null);
 		event.acknowledge().subscribe();
-
-		 */
 	}
 }
