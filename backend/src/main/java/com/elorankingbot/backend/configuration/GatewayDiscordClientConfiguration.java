be3389a903e64cbf4a66cab0d2b347bdfda9509e
Jakob Heiden <JakobHeiden@gmx.de>
2022-05-23 12:15:45 +0200

    try fix NativeIoException issue

--- backend/src/main/java/com/elorankingbot/backend/configuration/GatewayDiscordClientConfiguration.java
+++ backend/src/main/java/com/elorankingbot/backend/configuration/GatewayDiscordClientConfiguration.java
@@ -1,6 +1,7 @@
 package com.elorankingbot.backend.configuration;
 
 import com.elorankingbot.backend.service.Services;
+import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
@@ -8,9 +9,13 @@ import discord4j.core.object.entity.User;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
 import discord4j.core.object.presence.Status;
+import discord4j.rest.request.RouteMatcher;
+import discord4j.rest.response.ResponseFunction;
+import io.netty.channel.unix.Errors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.retry.Retry;
 
 @Slf4j
 @Configuration
@@ -23,6 +28,9 @@ public class GatewayDiscordClientConfiguration {
 				: System.getenv("DISCORD_BOT_TOKEN");
 		GatewayDiscordClient client = DiscordClientBuilder
 				.create(botToken)
+				.onClientResponse(ResponseFunction.retryWhen(
+						RouteMatcher.any(),
+						Retry.anyOf(Errors.NativeIoException.class)))
 				.build()
 				.login()
 				.block();
