d26680c3def97ad0f0acfbd54b068ec5a80073b2
Jakob Heiden <JakobHeiden@gmx.de>
2022-07-12 21:22:57 +0200

    bandaid for permissions in dispute

--- backend/src/main/java/com/elorankingbot/backend/commands/mod/dispute/RuleAsCancel.java
+++ backend/src/main/java/com/elorankingbot/backend/commands/mod/dispute/RuleAsCancel.java
@@ -1,8 +1,10 @@
 package com.elorankingbot.backend.commands.mod.dispute;
 
 import com.elorankingbot.backend.model.MatchResult;
+import com.elorankingbot.backend.model.Player;
 import com.elorankingbot.backend.service.MatchService;
 import com.elorankingbot.backend.service.Services;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 
 public class RuleAsCancel extends RuleAsWinOrDraw {
@@ -13,6 +15,24 @@ public class RuleAsCancel extends RuleAsWinOrDraw {
 
 	@Override
 	public void execute() {
+		boolean userIsAdmin = event.getInteraction().getMember().get()
+				.getRoleIds().stream().map(Snowflake::asLong).toList()
+				.contains(server.getAdminRoleId());
+		boolean userIsMod = event.getInteraction().getMember().get()
+				.getRoleIds().stream().map(Snowflake::asLong).toList()
+				.contains(server.getModRoleId());
+		boolean isAdjudicatingOwnMatch = match.containsPlayer(Player.generateId(server.getGuildId(),
+				event.getInteraction().getUser().getId().asLong()));
+		if (!userIsAdmin && !userIsMod) {
+			event.reply(String.format("Only <@&%s> and <@&%s> can adjudicate a match.",
+					server.getAdminRoleId(), server.getModRoleId())).withEphemeral(true).subscribe();
+			return;
+		}
+		if (isAdjudicatingOwnMatch && !userIsAdmin) {
+			event.reply("Moderators cannot adjudicate their own match.").withEphemeral(true).subscribe();
+			return;
+		}
+
 		String reason = String.format("%s has ruled the match to be canceled.", moderatorTag);
 		MatchResult canceledMatchResult = MatchService.generateCanceledMatchResult(match);
 		matchService.processMatchResult(canceledMatchResult, match, reason);
