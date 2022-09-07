75b1d2c7acb6055de8a80a36be18c3928cbbc956
Jakob Heiden <JakobHeiden@gmx.de>
2022-09-07 19:37:33 +0200

    add invites to GuildInfo

--- backend/src/main/java/com/elorankingbot/backend/commands/owner/GuildInfo.java
+++ backend/src/main/java/com/elorankingbot/backend/commands/owner/GuildInfo.java
@@ -3,7 +3,9 @@ package com.elorankingbot.backend.commands.owner;
 import com.elorankingbot.backend.commands.SlashCommand;
 import com.elorankingbot.backend.service.Services;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.ExtendedInvite;
 import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.discordjson.json.ApplicationCommandData;
 import discord4j.discordjson.json.ApplicationCommandOptionData;
 import discord4j.discordjson.json.ApplicationCommandRequest;
@@ -36,12 +38,20 @@ public class GuildInfo extends SlashCommand {
 		try {
 			long guildId = Long.parseLong(event.getOption("guildid").get().getValue().get().asString());
 			Guild guild = bot.getGuildById(guildId).block();
-			String reply = String.format("%s:%s", guildId, guild.getName());
+			String reply = String.format("%s:%s:%s\n", guildId, guild.getName(), guild.getMemberCount());
 			List<ApplicationCommandData> guildCommands = bot.getAllGuildCommands(guildId).block();
 			for (ApplicationCommandData guildCommand : guildCommands) {
-				reply += "\n" + guildCommand.name();
+				reply += guildCommand.name() + ", ";
+			}
+			List<ExtendedInvite> invites = guild.getInvites().buffer().blockLast();
+			if (invites != null) {
+				for (ExtendedInvite invite : invites) {
+					reply += "\nhttps://discord.gg/" + invite.getCode();
+				}
 			}
 			event.reply(reply).withEphemeral(true).doOnError(super::forwardToEventParser).subscribe();
+		} catch (NumberFormatException e) {
+			event.reply("That's not a number").withEphemeral(true).subscribe();
 		} catch (Exception e) {
 			event.reply(e.getMessage()).withEphemeral(true)
 					.doOnError(super::forwardToEventParser).subscribe();
