b6202bac2c759afa9c11d202de33aef5f3986a26
JakobHeiden <JakobHeiden@gmx.de>
2023-03-02 19:50:42 +0100

    make AllGuilds able to handle 2000+ characters

--- backend/src/main/java/com/elorankingbot/backend/commands/owner/AllGuilds.java
+++ backend/src/main/java/com/elorankingbot/backend/commands/owner/AllGuilds.java
@@ -14,38 +14,39 @@ import java.util.*;
 @OwnerCommand
 public class AllGuilds extends SlashCommand {
 
-	public AllGuilds(ChatInputInteractionEvent event, Services services) {
-		super(event, services);
-	}
+    public AllGuilds(ChatInputInteractionEvent event, Services services) {
+        super(event, services);
+    }
 
-	public static ApplicationCommandRequest getRequest() {
-		return ApplicationCommandRequest.builder()
-				.name(AllGuilds.class.getSimpleName().toLowerCase())
-				.description(AllGuilds.class.getSimpleName())
-				.defaultPermission(true)
-				.build();
-	}
+    public static ApplicationCommandRequest getRequest() {
+        return ApplicationCommandRequest.builder()
+                .name(AllGuilds.class.getSimpleName().toLowerCase())
+                .description(AllGuilds.class.getSimpleName())
+                .defaultPermission(true)
+                .build();
+    }
 
-	protected void execute() throws Exception {
-		event.deferReply().subscribe();
-		try {
-			Map<Integer, String> guilds = new HashMap<>();
-			for (Server server : dbService.findAllServers()) {
-				Guild guild;
-				try {
-					guild = bot.getGuild(server).block();
-					guilds.put(guild.getMemberCount(), String.format("%s - %s:%s", guild.getMemberCount(), guild.getId().asString(), guild.getName()));
-				} catch (ClientException ignored) {
-				}
-			}
-			List<Integer> sortedKeys = new ArrayList<>(guilds.keySet().stream().toList());
-			Collections.sort(sortedKeys);
-			event.createFollowup(String.format("Guilds in total: %s\n", bot.getAllGuildIds().size()) +
-					String.join("\n", sortedKeys.stream().map(guilds::get).toList())).withEphemeral(true).subscribe();
-		} catch (Exception e) {
-			e.printStackTrace();
-			event.createFollowup(e.getMessage()).withEphemeral(true)
-					.subscribe(NO_OP, super::forwardToExceptionHandler);
-		}
-	}
+    protected void execute() throws Exception {
+        event.deferReply().withEphemeral(true).subscribe();
+        try {
+            StringBuilder reply = new StringBuilder();
+            int numGuilds = 0;
+            for (Server server : dbService.findAllServers()) {
+                try {
+                    Guild guild = bot.getGuild(server).block();
+                    numGuilds++;
+                    reply.append(String.format("%s - %s:%s\n", guild.getMemberCount(), guild.getId().asString(), guild.getName()));
+                    if (reply.length() > 1800) {// 2000 is limit
+                        event.createFollowup(reply.toString()).subscribe();
+                        reply = new StringBuilder();
+                    }
+                } catch (ClientException ignored) {// guilds that the server has lost access to end up here
+                }
+            }
+            event.createFollowup(reply.append("Total number of guilds: " + numGuilds).toString()).subscribe();
+        } catch (Exception e) {
+            e.printStackTrace();
+            event.createFollowup(e.getMessage()).subscribe(NO_OP, super::forwardToExceptionHandler);
+        }
+    }
 }
