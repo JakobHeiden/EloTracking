900ab6d207c2bfdd24c711286dd03db5e61158b7
JakobHeiden <JakobHeiden@gmx.de>
2023-04-03 18:01:56 +0200

    update to Discord4J 3.2.4

--- backend/src/main/java/com/elorankingbot/backend/configuration/GatewayDiscordClientConfiguration.java
+++ backend/src/main/java/com/elorankingbot/backend/configuration/GatewayDiscordClientConfiguration.java
@@ -6,6 +6,8 @@ import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.retriever.RestEntityRetriever;
 import discord4j.core.retriever.StoreEntityRetriever;
+import discord4j.gateway.intent.Intent;
+import discord4j.gateway.intent.IntentSet;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +16,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayDiscordClientConfiguration {
 
-	@Bean
-	public GatewayDiscordClient createClient(Services services) {
-		String botToken = services.props.isUseDevBotToken() ?
-				System.getenv("DEV_BOT_TOKEN")
-				: System.getenv("DISCORD_BOT_TOKEN");
-		GatewayDiscordClient client = DiscordClient.create(botToken)
-				.gateway()
-				.setEntityRetrievalStrategy(gateway -> new CustomFallbackEntityRetrieverWithAddedCacheMissLogging(
-						new StoreEntityRetriever(gateway), new RestEntityRetriever(gateway)))
-						.login().block();
-		return client;
-	}
+    @Bean
+    public GatewayDiscordClient createClient(Services services) {
+        String botToken = services.props.isUseDevBotToken() ?
+                System.getenv("DEV_BOT_TOKEN")
+                : System.getenv("DISCORD_BOT_TOKEN");
+        GatewayDiscordClient client = DiscordClient.create(botToken)
+                .gateway()
+                .setEntityRetrievalStrategy(gateway -> new CustomFallbackEntityRetrieverWithAddedCacheMissLogging(
+                        new StoreEntityRetriever(gateway), new RestEntityRetriever(gateway)))
+                .setEnabledIntents(IntentSet.nonPrivileged().andNot(IntentSet.of(Intent.MESSAGE_CONTENT)))
+                .login().block();
+        return client;
+    }
 }
