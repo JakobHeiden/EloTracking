eb3b7cbf0c142661daff2784edd31204310c2878
JakobHeiden <JakobHeiden@gmx.de>
2023-04-18 16:59:05 +0200

    remove long absolete web frontend junk. cherrypicked from branch dread

--- frontend/src/service/LeaderboardService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getRankings = (channelId) =>
-    axios.get('/api/rankings/' + channelId)
-        .then(response => response.data);
-
-export const getGameData = (channelId) =>
-    axios.get('api/gamedata/' + channelId)
-        .then(response => response.data);
